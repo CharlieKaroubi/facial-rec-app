b18b406bcca213cb3ce922392da41e554e183e89
Charlie Karoubi <charliekaroubi@gmail.com>
2025-01-21 13:25:30 -0600

    Added initial components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className = "lora">
+            <div className='white f3'>
+                {'Charlie, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
